5b094b0cfcbe27a7025a042658cba568564e4c71
griddevconnet <grid.devconnet@gmail.com>
2026-09-20 12:53:49 +0000

    update

--- src/pages/Membership.jsx
+++ src/pages/Membership.jsx
@@ -1,6 +1,5 @@
-import { Navigate } from 'react-router-dom'
 import MembershipSection from '../components/Membership.jsx'
 
 export default function Membership() {
-  return <Navigate to="/membership/apply" replace />
+  return <MembershipSection />
 }
